a47dd21a6012a6a467d007a7b645b75ded4defe3
Ethan C Collins <ethan.collins335@myci.csuci.edu>
2018-11-07 23:32:51 -0800

    First commit of our C# application

---
+++ Solution/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PraticeAppSol.Models;
+
+namespace PraticeAppSol.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Random()
+        {
+            var movie = new Movie {Id = 80082, Name = "Ethan is the best"};
+
+            return View(movie);
+        }
+        
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ Solution/Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace PraticeAppSol.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
